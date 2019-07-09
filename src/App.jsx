ab2ee9873e468384abc4260bac0d3781e25c9889
Alex Permyakov <alex.permyakoff@gmail.com>
2019-07-09 18:40:08 +0200

    add live-code editor

--- src/App.jsx
+++ src/App.jsx
@@ -21,15 +21,42 @@ const Settings = styled.section.attrs({ className: "columns" })`
   margin: 0 auto;
 `;
 
+const getPrintedCode = (options, n = 1) => {
+  let text = "";
+  let { length } = Object.keys(options);
+  let i = 0;
+  for (let k in options) {
+    let comma = i++ < length - 1 ? "," : "";
+    let val = options[k];
+    const offset = "  ".repeat(n);
+    let pval = typeof val === "string" ? `"${val}"` : val;
+    pval =
+      typeof pval === "object"
+        ? `{\n${getPrintedCode(pval, n + 1)}${offset}}`
+        : pval;
+    text += `${offset}${k}: ${pval}${comma}\n`;
+  }
+
+  return text;
+};
+
+const getConfigureExample = options =>
+  "notifier.configure({\n" + getPrintedCode(options) + "});";
+
 const App = () => {
-  const configure = `notifier.configure({
+  const options = {
+    autoClose: 2000,
     position: "top-center",
+    delay: 500,
+    single: false,
+    containerWidth: "480px",
     animation: {
       in: "fadeIn",
       out: "fadeOut",
       duration: 400
     }
-  });`;
+  };
+
   return (
     <>
       <section className="section">
@@ -43,7 +70,7 @@ const App = () => {
           <h1 className="subtitle">Options</h1>
         </div>
         <div className="column">
-          <Pre>{configure}</Pre>
+          <Pre>{getConfigureExample(options)}</Pre>
         </div>
       </Settings>
     </>
