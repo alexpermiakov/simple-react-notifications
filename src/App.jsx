137157cf0c9b401ca6de8c14e52011ca3e5d8509
Alex Permyakov <alex.permyakoff@gmail.com>
2019-07-10 22:51:05 +0200

    add settings for animation

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ const Settings = styled.section.attrs({ className: "columns" })`
       line-height: 36px;
       height: 36px;
       display: inline-block;
-      width: 100px;
+      width: 130px;
     }
   }
   .input {
@@ -50,6 +50,10 @@ const Settings = styled.section.attrs({ className: "columns" })`
   .select select {
     width: 130px;
   }
+  .textarea {
+    height: 250px;
+    cursor: default !important;
+  }
 `;
 
 const H3 = styled.h3`
@@ -57,13 +61,57 @@ const H3 = styled.h3`
   margin-bottom: 12px;
 `;
 
-const message = "Your items have been updated.";
+const message = "You have a new message.";
 
 const getConfigureExample = options =>
   "notifier.configure({\n" + getPrintedCode(options) + "});";
 
 const getNotificationExample = options =>
   `notifier.success("${message}", {\n${getPrintedCode(options)}});`;
+
+const getCustomComponent = options => {
+  options.render = `({ id, onClose }) => (
+    <RouteInfo
+      key={id}
+      onClosePanel={onClose}
+      header={"The shortest way to ride home."}
+    />
+  )`;
+  return `notifier({
+${getPrintedCode(options, 1, true)}})`;
+};
+
+const componentExample = `const RouteInfo = ({(header, onClosePanel)}) => (
+  <div className="route-info" onClick={onClosePanel}>
+    <h3>{header}</h3>
+    <p>
+      Bicycle 2.4 km, 8 min.
+    </p>
+    <p>
+      Use caution - may involve errors or
+      sections not suited for bicycling
+    </p>
+  </div>
+  );`;
+
+const RouteInfo = ({ header, onClosePanel }) => (
+  <div
+    className="route-info"
+    onClick={onClosePanel}
+    style={{
+      height: "250px",
+      background: "white",
+      color: "black",
+      padding: "8px 16px",
+      position: "relative",
+      boxShadow: "0 1px 10px 0 rgba(0, 0, 0, 0.1)"
+    }}
+  >
+    <h3 className="subtitle">{header}</h3>
+    <p>Bicycle 2.4 km, 8 min.</p>
+    <p>Use caution - may involve errors or sections not suited for bicycling</p>
+  </div>
+);
 
 const App = () => {
   const [type, setType] = useState("success");
@@ -72,17 +120,13 @@ const App = () => {
     position: "top-right",
     delay: 0,
     single: false,
-    containerWidth: "480px",
-    animation: {
-      in: "fadeIn",
-      out: "fadeOut",
-      duration: 400
-    }
+    animation: false
   });
+  const [animationEnabled, setAnimation] = useState(options.animation);
 
   useEffect(() => notifier.configure(options), [options]);
 
-  const { position, autoClose, single, delay } = options;
+  const { position, autoClose, single, delay, animation } = options;
 
   return (
     <>
@@ -95,8 +139,31 @@ const App = () => {
       </Header>
       <Settings>
         <div className="column">
-          <H3>Options</H3>
           <div>
+            <button
+              className="button is-primary"
+              onClick={() => {
+                if (type === "success" || type === "error") {
+                  notifier[type](message);
+                } else {
+                  notifier({
+                    render: ({ id, onClose }) => (
+                      <RouteInfo
+                        key={id}
+                        onClosePanel={onClose}
+                        header={"The shortest way to ride home."}
+                      />
+                    )
+                  });
+                }
+              }}
+            >
+              Click to show!
+            </button>
+            <br />
+            <br />
+
+            <H3>Options</H3>
             <div className="item">
               <span>position</span>
               <div className="select">
@@ -130,43 +197,49 @@ const App = () => {
               </div>
             </div>
 
+            {type === "custom" && (
+              <div className="item">
+                <span>Component</span>
+                <textarea className="textarea" disabled={true}>
+                  {componentExample}
+                </textarea>
+              </div>
+            )}
+
             <div className="item">
-              <label className="checkbox">
-                <span>autoClose</span>
-                <input
-                  className="input"
-                  type="number"
-                  value={autoClose}
-                  onChange={() => {
-                    setOptions({
-                      ...options,
-                      autoClose: autoClose ? autoClose : false
-                    });
-                  }}
-                />
-              </label>
+              <span>autoClose</span>
+              <input
+                className="input"
+                type="number"
+                value={autoClose}
+                onChange={({ target }) => {
+                  setOptions({
+                    ...options,
+                    autoClose: target.value ? +target.value : false
+                  });
+                }}
+              />
             </div>
 
             <div className="item">
-              <label>
-                <span>delay</span>
-                <input
-                  className="input"
-                  type="number"
-                  onChange={() => {
-                    setOptions({
-                      ...options,
-                      delay
-                    });
-                  }}
-                />
-              </label>
+              <span>delay</span>
+              <input
+                className="input"
+                type="number"
+                value={delay}
+                onChange={({ target }) => {
+                  setOptions({
+                    ...options,
+                    delay: +target.value
+                  });
+                }}
+              />
             </div>
 
             <div className="item">
               <label className="checkbox">
                 <span style={{ height: "20px", lineHeight: "20px" }}>
-                  single
+                  onlyLast
                 </span>
                 <input
                   type="checkbox"
@@ -182,30 +255,126 @@ const App = () => {
               </label>
             </div>
 
-            <br />
-            <button
-              className="button is-primary"
-              onClick={() => {
-                if (type === "success" || type === "error") {
-                  notifier[type](message);
-                }
-              }}
-            >
-              Click to show!
-            </button>
+            <div className="item">
+              <span style={{ height: "20px", lineHeight: "20px" }}>
+                animation
+              </span>
+              <input
+                type="checkbox"
+                checked={animationEnabled}
+                value={animationEnabled}
+                onChange={() => {
+                  setAnimation(!animationEnabled);
+                  setOptions({
+                    ...options,
+                    animation: !animationEnabled
+                      ? {
+                          in: "fadeIn",
+                          out: "fadeOut",
+                          duration: animation.duration || 600
+                        }
+                      : false
+                  });
+                }}
+              />
+            </div>
+
+            {animationEnabled && (
+              <div className="item">
+                <label className="checkbox">
+                  <span>duration</span>
+                  <input
+                    className="input"
+                    type="number"
+                    value={animation.duration}
+                    onChange={({ target }) => {
+                      setOptions({
+                        ...options,
+                        animation: {
+                          in: "fadeIn",
+                          out: "fadeOut",
+                          duration: +target.value
+                        }
+                      });
+                    }}
+                  />
+                </label>
+              </div>
+            )}
+
+            {animationEnabled && (
+              <div className="item">
+                <span>in</span>
+                <div className="select">
+                  <select
+                    onChange={({ target }) => {
+                      setOptions({
+                        ...options,
+                        animation: {
+                          ...animation,
+                          in: target.value
+                        }
+                      });
+                    }}
+                    value={animation.in}
+                  >
+                    <option value="fadeIn">fadeIn</option>
+                    <option value="zoomIn">zoomIn</option>
+                  </select>
+                </div>
+              </div>
+            )}
+
+            {animationEnabled && (
+              <div className="item">
+                <span>out</span>
+                <div className="select">
+                  <select
+                    onChange={({ target }) => {
+                      setOptions({
+                        ...options,
+                        animation: {
+                          ...animation,
+                          out: target.value
+                        }
+                      });
+                    }}
+                    value={animation.out}
+                  >
+                    <option value="fadeOut">fadeOut</option>
+                    <option value="zoomOut">zoomOut</option>
+                  </select>
+                </div>
+              </div>
+            )}
           </div>
         </div>
         <div className="column">
-          <H3>Configuration (optional)</H3>
+          <H3>
+            <b>Configuration</b> (optional, common for all notifications)
+          </H3>
           <Pre>{getConfigureExample(options)}</Pre>
           <br />
-          <H3>Show notification</H3>
-          <Pre>
-            {getNotificationExample({
-              position,
-              autoClose
-            })}
-          </Pre>
+          <H3>
+            <b>Show notification</b> (you can override configuration here)
+          </H3>
+          {type !== "custom" && (
+            <Pre>
+              {getNotificationExample({
+                position,
+                autoClose
+              })}
+            </Pre>
+          )}
+
+          {type === "custom" && (
+            <Pre>
+              {getCustomComponent({
+                position,
+                autoClose
+              })}
+            </Pre>
+          )}
         </div>
       </Settings>
     </>
--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,17 @@
-export const getPrintedCode = (options, n = 1) => {
+export const getPrintedCode = (options, n = 1, avoidQuotes) => {
   let text = "";
+
+  if (!options.animation) {
+    delete options.animation;
+  }
+
   let { length } = Object.keys(options);
   let i = 0;
   for (let k in options) {
     let comma = i++ < length - 1 ? "," : "";
     let val = options[k];
     const offset = "  ".repeat(n);
-    let pval = typeof val === "string" ? `"${val}"` : val;
+    let pval = typeof val === "string" && !avoidQuotes ? `"${val}"` : val;
     pval =
       typeof pval === "object"
         ? `{\n${getPrintedCode(pval, n + 1)}${offset}}`
